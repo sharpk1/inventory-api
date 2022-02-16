260246b55d3fbe0462a98d0f0f318daf19502429
Kush Shah <Kush.Shah@neudesic.com>
2022-02-15 19:09:36 -0600

    initial commit

---
+++ WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using Bookstore.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookServices _bookServices;
+        public BooksController(IBookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(_bookServices.GetBooks());
+        }
+    }
+}
